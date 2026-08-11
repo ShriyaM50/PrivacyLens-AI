ecca145414bbe5e66f5a9e1210037393d50eccb4
Shriya Mittal <shriya1538.be24@chitkarauniversity.edu.in>
2026-08-11 10:24:01 +0530

    Add popup styling

--- popup/popup.js
+++ popup/popup.js
@@ -1,4 +1,5 @@
 const fileInput = document.getElementById('file-input');
+const output = document.getElementById('output');
 
 fileInput.addEventListener('change', async (event) => {
   const file = event.target.files[0];
@@ -6,4 +7,15 @@ fileInput.addEventListener('change', async (event) => {
 
   const text = await file.text();
   console.log(text);
+
+  const emailPattern = /\S+@\S+\.\S+/;
+  const found = text.match(emailPattern);
+  console.log(found);
+
+  const phonePattern = /\d{10}/;
+  const foundPhone = text.match(phonePattern);
+  console.log(foundPhone);
+
+  const results = "Email: " + found + "\nPhone: " + foundPhone;
+  output.textContent = results;
 });
